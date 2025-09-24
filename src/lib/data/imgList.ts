e5ca9b015e8578b3bcd95a2cac8852dd856d0d54
seonyoungg <tjsduddlfma1@naver.com>
2025-09-24 20:11:00 +0900

    💄 style: 부트스트랩 기술 제거

--- src/lib/data/imgList.ts
+++ src/lib/data/imgList.ts
@@ -1,44 +1,40 @@
-import { ImgListProps } from "@/types/common";
+import { ImgListProps } from '@/types/common';
 
-export const IconList:ImgListProps[] = [
+export const IconList: ImgListProps[] = [
   {
-    title:"HTML",
-    imgUrl : "img/icon/HTML.svg"
+    title: 'HTML',
+    imgUrl: 'img/icon/HTML.svg',
   },
   {
-    title:"CSS",
-    imgUrl : "img/icon/CSS.svg"
+    title: 'CSS',
+    imgUrl: 'img/icon/CSS.svg',
   },
   {
-    title:"Bootstrap",
-    imgUrl : "img/icon/Bootstrap.svg"
+    title: 'TailwindCSS',
+    imgUrl: 'img/icon/TailwindCSS-Dark.svg',
   },
   {
-    title:"TailwindCSS",
-    imgUrl : "img/icon/TailwindCSS-Dark.svg"
+    title: 'JavaScript',
+    imgUrl: 'img/icon/JavaScript.svg',
   },
   {
-    title:"JavaScript",
-    imgUrl : "img/icon/JavaScript.svg"
+    title: 'JQuery',
+    imgUrl: 'img/icon/JQuery.svg',
   },
   {
-    title:"JQuery",
-    imgUrl : "img/icon/JQuery.svg"
+    title: 'TypeScript',
+    imgUrl: 'img/icon/TypeScript.svg',
   },
   {
-    title:"TypeScript",
-    imgUrl : "img/icon/TypeScript.svg"
+    title: 'React',
+    imgUrl: 'img/icon/React-Dark.svg',
   },
   {
-    title:"React",
-    imgUrl : "img/icon/React-Dark.svg"
+    title: 'NextJS',
+    imgUrl: 'img/icon/NextJS-Dark.svg',
   },
   {
-    title:"NextJS",
-    imgUrl : "img/icon/NextJS-Dark.svg"
+    title: 'Figma',
+    imgUrl: 'img/icon/Figma-Dark.svg',
   },
-  {
-    title:"Figma",
-    imgUrl : "img/icon/Figma-Dark.svg"
-  },
-]
+];
